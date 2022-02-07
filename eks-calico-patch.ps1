8230815e6a3fb07e99a85c12736f383f560eee61
Christoph Heuwieser <dropdead@hashcat.net>
2022-02-08 00:33:30 +0100

    Update eks-calico-patch.ps1

--- eks-calico-patch.ps1
+++ eks-calico-patch.ps1
@@ -3,4 +3,4 @@ $file = 'c:\install-calico-windows.ps1'
 $regex = '(ArgumentList.*)'
 (get-content $file) -replace $regex, 'Argument @{ PathName, $UpdatedPath}' |set-content $file
 Write-Host "executing to c:\install-calico-windows.ps1..."
-c:\install-calico-windows.ps1
+& c:\install-calico-windows.ps1
