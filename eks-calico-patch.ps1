54f0f32fc52f8328fe9a836748206588c88df2ac
Christoph Heuwieser <dropdead@hashcat.net>
2022-03-18 11:53:35 +0100

    Update eks-calico-patch.ps1

--- eks-calico-patch.ps1
+++ eks-calico-patch.ps1
@@ -2,6 +2,8 @@ Write-Host "applying patch to c:\install-calico-windows.ps1..."
 $file = 'c:\install-calico-windows.ps1'
 $regex = '(ArgumentList.*)'
 (get-content $file) -replace $regex, 'Argument @{ PathName = $UpdatedPath }' |set-content $file
+$regex = '(\$supportsDSR =.*)'
+(get-content $file) -replace $regex, '$supportsDSR =(($OSInfo.WindowsVersion -as [int]) -GE 1903 -And ($OSInfo.OsBuildNumber -as [int]) -GE 18317)' |set-content $file
 Write-Host "finished, execute install-calico-windows.ps1 now"
 
 & c:\install-calico-windows.ps1
